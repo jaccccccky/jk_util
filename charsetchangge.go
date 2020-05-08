bdaefb1d639fa88005de025ab2b9408ea159400e
jaccccccky <jing476@qq.com>
2020-05-08 10:39:27 +0800

    test edit

--- charsetchangge.go
+++ charsetchangge.go
@@ -2,7 +2,7 @@ package jk_util
 
 import "github.com/axgle/mahonia"
 
-/*ConvertToString 用来做字符编码转换,测试编辑1*/
+/*ConvertToString 用来做字符编码转换,测试编辑2*/
 func ConvertToString(src string, srcCode string, tagCode string) string {
 	srcCoder := mahonia.NewDecoder(srcCode)
 	srcResult := srcCoder.ConvertString(src)
