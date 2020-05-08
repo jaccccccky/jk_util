ba0dd811a06d984f354f6b511e6a0c77e9b251fd
jaccccccky <jing476@qq.com>
2020-05-08 10:42:54 +0800

    Signed-off-by: jaccccccky <jing476@qq.com>

--- charsetchangge.go
+++ charsetchangge.go
@@ -2,7 +2,7 @@ package jk_util
 
 import "github.com/axgle/mahonia"
 
-/*ConvertToString 用来做字符编码转换,测试编辑2*/
+/*ConvertToString 用来做字符编码转换,测试编辑3*/
 func ConvertToString(src string, srcCode string, tagCode string) string {
 	srcCoder := mahonia.NewDecoder(srcCode)
 	srcResult := srcCoder.ConvertString(src)
