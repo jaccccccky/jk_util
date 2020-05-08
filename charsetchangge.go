62226700734be910a36d3be5500dad7dd640036b
jaccccccky <64696887+jaccccccky@users.noreply.github.com>
2020-05-08 10:32:24 +0800

    Create charsetchangge.go

---
+++ charsetchangge.go
@@ -0,0 +1,13 @@
+package jk_util
+
+import "github.com/axgle/mahonia"
+
+/*ConvertToString 用来做字符编码转换*/
+func ConvertToString(src string, srcCode string, tagCode string) string {
+	srcCoder := mahonia.NewDecoder(srcCode)
+	srcResult := srcCoder.ConvertString(src)
+	tagCoder := mahonia.NewDecoder(tagCode)
+	_, cdata, _ := tagCoder.Translate([]byte(srcResult), true)
+	result := string(cdata)
+	return result
+}
